b372dcfb29531c9f21ceb902f003681789d5aa9b
mcflugen <mcflugen@gmail.com>
2018-10-25 15:59:10 -0600

    Set __all__ to include all pymt classes.

--- pymt_{{cookiecutter.plugin_name}}/pymt_{{cookiecutter.plugin_name}}/bmi.py
+++ pymt_{{cookiecutter.plugin_name}}/pymt_{{cookiecutter.plugin_name}}/bmi.py
@@ -1,12 +1,33 @@
 from __future__ import absolute_import
 
-{% for entry_point in cookiecutter.entry_points.split(',') %}
+{% set classes = [] -%}
+{%- for entry_point in cookiecutter.entry_points.split(',') -%}
     {%- set pymt_class = entry_point.split('=')[0] -%}
-    {%- set plugin_module, plugin_class = entry_point.split('=')[1].split(':') -%}
-    {%- if cookiecutter.language == 'c' or cookiecutter.language == 'c++' %}
-from .lib import {{ pymt_class }}
-    {%- else %}
+    {% set _ = classes.append(pymt_class) %}
+{%- endfor -%}
+
+{%- if cookiecutter.language == 'c' or cookiecutter.language == 'c++' %}
+
+from .lib import {{ classes|join(', ') }}
+
+{%- else %}
+
+    {% for entry_point in cookiecutter.entry_points.split(',') %}
+        {%- set pymt_class = entry_point.split('=')[0] -%}
+        {%- set plugin_module, plugin_class = entry_point.split('=')[1].split(':') -%}
+
 from {{ plugin_module }} import {{ plugin_class }} as {{ pymt_class }}
-{{ pymt_class }}.__name__ = "{{ pymt_class }}"
-    {%- endif %}
+
+    {%- endfor %}
+
+    {%- for cls in classes %}
+{{ cls }}.__name__ = "{{ cls }}"
+    {%- endfor %}
+
+{%- endif %}
+
+__all__ = [
+{%- for cls in classes %}
+    "{{ cls }}",
 {%- endfor %}
+]
