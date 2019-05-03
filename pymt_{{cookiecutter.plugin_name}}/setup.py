c1e7f70803500e73e91a55ed505b2f9660a05593
Mark Piper <mark.piper@colorado.edu>
2019-05-03 17:20:21 -0600

    Expand relative include paths
    If a relative include_dir is supplied in the plugin.yaml file, expand
    it into an absolute path under the include directory of the Python
    installation. This only affects wrapped Fortran code.

--- pymt_{{cookiecutter.plugin_name}}/setup.py
+++ pymt_{{cookiecutter.plugin_name}}/setup.py
@@ -108,6 +108,8 @@ def build_interoperability():
     cmd.append(compiler.compile_switch)
     cmd.append('-fPIC')
     for include_dir in common_flags['include_dirs']:
+        if os.path.isabs(include_dir) is False:
+            include_dir = os.path.join(sys.prefix, "include", include_dir)
         cmd.append('-I{}'.format(include_dir))
     cmd.append('bmi_interoperability.f90')
 
