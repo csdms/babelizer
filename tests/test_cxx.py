d0a8fe021b63e89039c71ab2fb6bcf4b0fd84c04
mcflugen <mcflugen@gmail.com>
2020-09-30 15:36:24 -0600

    add test for babelizing a c++ library

---
+++ tests/test_cxx.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+import os
+import pathlib
+import shutil
+import subprocess
+
+from click.testing import CliRunner
+
+from babelizer.cli import babelize
+
+
+def test_babelize_init_cxx(tmpdir, datadir):
+    runner = CliRunner()
+
+    with tmpdir.as_cwd():
+        shutil.copy(datadir / "babel.toml", ".")
+        result = runner.invoke(babelize, ["init", "babel.toml", "."])
+
+        assert result.exit_code == 0
+        assert pathlib.Path("pymt_heat").exists()
+        assert (pathlib.Path("pymt_heat") / "babel.toml").is_file()
+
+        try:
+            result = subprocess.run(
+                ["pip", "install", "-e", "."],
+                cwd="pymt_heat",
+                check=True,
+                text=True,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.STDOUT,
+            )
+        except subprocess.CalledProcessError as err:
+            assert err.output is None, err.output
+
+        assert result.returncode == 0
+
+        os.mkdir("_test")
+        shutil.copy(datadir / "config.txt", "_test/")
+
+        try:
+            result = subprocess.run(
+                ["bmi-test", "--config-file=config.txt", "--root-dir=.", "pymt_heat:HeatBMI", "-vvv"],
+                cwd="_test",
+                check=True,
+                text=True,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.STDOUT,
+            )
+        except subprocess.CalledProcessError as err:
+            assert err.output is None, err.output
+
+        assert result.returncode == 0
