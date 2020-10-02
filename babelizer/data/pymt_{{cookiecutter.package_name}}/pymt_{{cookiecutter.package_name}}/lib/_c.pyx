a924a5e659fa398974b792b31430b07708bbe402
mcflugen <mcflugen@gmail.com>
2020-10-02 10:20:52 -0600

    remove bmi.h, bmi.c; call bmi methods in pyx file

--- babelizer/data/pymt_{{cookiecutter.package_name}}/pymt_{{cookiecutter.package_name}}/lib/_c.pyx
+++ babelizer/data/pymt_{{cookiecutter.package_name}}/pymt_{{cookiecutter.package_name}}/lib/_c.pyx
@@ -23,64 +23,66 @@ DTYPE_C_TO_PY = {
 
 cdef extern from "bmi.h":
     ctypedef struct Bmi:
-        pass
+        # pass
+        int (*initialize)(void *self, const char *config_file)
+        int (*update)(void *self);
+        int (*update_until)(void *self, double then);
+        int (*finalize)(void *self);
 
+        # Exchange items
+        int (*get_component_name)(void *self, char *name);
+        int (*get_input_item_count)(void *self, int *count);
+        int (*get_output_item_count)(void *self, int *count);
+        int (*get_input_var_names)(void *self, char **names);
+        int (*get_output_var_names)(void *self, char **names);
 
-cdef extern from "bmi.c":
-    int bmi_initialize(Bmi* model, const char *config_file)
-    int bmi_update(Bmi* model)
-    int bmi_update_until(Bmi* model, double until)
-    # int bmi_update_frac(Bmi* model, double frac)
-    int bmi_finalize(Bmi* model)
-    # int bmi_run_model(Bmi* model)
+        # Variable information
+        int (*get_var_grid)(void *self, const char *name, int *grid);
+        int (*get_var_type)(void *self, const char *name, char *type);
+        int (*get_var_units)(void *self, const char *name, char *units);
+        int (*get_var_itemsize)(void *self, const char *name, int *size);
+        int (*get_var_nbytes)(void *self, const char *name, int *nbytes);
+        int (*get_var_location)(void *self, const char *name, char *location);
 
-    int bmi_get_component_name(Bmi* model, char* name)
-    int bmi_get_input_item_count(Bmi* model, int *count)
-    int bmi_get_output_item_count(Bmi* model, int *count)
+        # Time information
+        int (*get_current_time)(void *self, double *time);
+        int (*get_start_time)(void *self, double *time);
+        int (*get_end_time)(void *self, double *time);
+        int (*get_time_units)(void *self, char *units);
+        int (*get_time_step)(void *self, double *time_step);
 
-    int bmi_get_input_var_names(Bmi* model, char **names)
-    int bmi_get_output_var_names(Bmi* model, char **names)
+        # Getters
+        int (*get_value)(void *self, const char *name, void *dest);
+        int (*get_value_ptr)(void *self, const char *name, void **dest_ptr);
+        int (*get_value_at_indices)(void *self, const char *name, void *dest, int *inds, int count);
 
-    int bmi_get_var_grid(Bmi* model, const char *name, int *gid)
-    int bmi_get_var_type(Bmi* model, const char *name, char *dtype)
-    int bmi_get_var_units(Bmi* model, const char *name, char *units)
-    int bmi_get_var_itemsize(Bmi* model, const char *name, int *itemsize)
-    int bmi_get_var_nbytes(Bmi* model, const char *name, int *nbytes)
-    int bmi_get_var_location(Bmi* model, const char *name, char *location)
+        # Setters
+        int (*set_value)(void *self, const char *name, void *src);
+        int (*set_value_at_indices)(void *self, const char *name, int *inds, int count, void *src);
 
-    int bmi_get_current_time(Bmi* model, double* time)
-    int bmi_get_start_time(Bmi* model, double* time)
-    int bmi_get_end_time(Bmi* model, double* time)
-    int bmi_get_time_units(Bmi* model, char* units)
-    int bmi_get_time_step(Bmi* model, double* time)
+        # Grid information
+        int (*get_grid_rank)(void *self, int grid, int *rank);
+        int (*get_grid_size)(void *self, int grid, int *size);
+        int (*get_grid_type)(void *self, int grid, char *type);
 
-    int bmi_get_value(Bmi* model, const char* name, void * dest)
-    int bmi_get_value_ptr(Bmi* model, const char* name, void ** ptr)
-    int bmi_get_value_at_indices(Bmi* model, const char* name, void * dest, int *inds, int count)
+        # Uniform rectilinear
+        int (*get_grid_shape)(void *self, int grid, int *shape);
+        int (*get_grid_spacing)(void *self, int grid, double *spacing);
+        int (*get_grid_origin)(void *self, int grid, double *origin);
 
-    int bmi_set_value(Bmi* model, const char* name, void *src)
-    int bmi_set_value_at_indices(Bmi* model, const char* name, int *inds, int count, void *src)
+        # Non-uniform rectilinear, curvilinear
+        int (*get_grid_x)(void *self, int grid, double *x);
+        int (*get_grid_y)(void *self, int grid, double *y);
+        int (*get_grid_z)(void *self, int grid, double *z);
 
-    int bmi_get_grid_rank(Bmi* model, int gid, int *rank)
-    int bmi_get_grid_size(Bmi* model, int gid, int *size)
-    int bmi_get_grid_type(Bmi* model, int gid, char *gtype)
-
-    int bmi_get_grid_shape(Bmi* model, int gid, int* shape)
-    int bmi_get_grid_spacing(Bmi* model, int gid, double* spacing)
-    int bmi_get_grid_origin(Bmi* model, int gid, double* origin)
-
-    int bmi_get_grid_x(Bmi *model, int grid, double *x)
-    int bmi_get_grid_y(Bmi *model, int grid, double *y)
-    int bmi_get_grid_z(Bmi *model, int grid, double *z)
-
-    int bmi_get_grid_node_count(Bmi *model, int grid, int *count)
-    int bmi_get_grid_edge_count(Bmi *model, int grid, int *count)
-    int bmi_get_grid_face_count(Bmi *model, int grid, int *count)
-
-    int bmi_get_grid_edge_nodes(Bmi *model, int grid, int *edge_nodes)
-    int bmi_get_grid_face_edges(Bmi *model, int grid, int *face_edges)
-    int bmi_get_grid_face_nodes(Bmi *model, int grid, int *face_nodes)
-    int bmi_get_grid_nodes_per_face(Bmi *model, int grid, int *nodes_per_face)
+        # Unstructured
+        int (*get_grid_node_count)(void *self, int grid, int *count);
+        int (*get_grid_edge_count)(void *self, int grid, int *count);
+        int (*get_grid_face_count)(void *self, int grid, int *count);
+        int (*get_grid_edge_nodes)(void *self, int grid, int *edge_nodes);
+        int (*get_grid_face_edges)(void *self, int grid, int *face_edges);
+        int (*get_grid_face_nodes)(void *self, int grid, int *face_nodes);
+        int (*get_grid_nodes_per_face)(void *self, int grid, int *nodes_per_face);
 
 
 def ok_or_raise(status):
@@ -90,7 +92,7 @@ def ok_or_raise(status):
 {%- for babelized_class, component in cookiecutter.components|dictsort %}
 # start: {{ babelized_class|lower }}.pyx
 
-cdef extern from "bmi.h":
+cdef extern from "{{ component.header }}":
     Bmi* {{ component.class }}(Bmi *model)
 
 
@@ -108,42 +110,38 @@ cdef class {{ babelized_class }}:
         else:
             {{ component.class }}(self._bmi)
 
+    def __dealloc__(self):
+        free(self._bmi)
+        self._bmi = NULL
+
     def initialize(self, config_file):
-        status = <int>bmi_initialize(self._bmi, <char*>config_file)
-        # status = <int>bmi_initialize(self._bmi, <char*>config_file, <void**>&(self._bmi))
+        status = <int>self._bmi.initialize(self._bmi, <char*>config_file)
         ok_or_raise(status)
 
     def update(self):
-        status = <int>bmi_update(self._bmi)
+        status = <int>self._bmi.update(self._bmi)
         ok_or_raise(status)
 
     def update_until(self, time):
-        status = <int>bmi_update_until(self._bmi, time)
+        status = <int>self._bmi.update_until(self._bmi, time)
         ok_or_raise(status)
 
-    # def update_frac(self, frac):
-    #     status = <int>bmi_update_frac(self._bmi, frac)
-    #     ok_or_raise(status)
-
-    # def run_model(self):
-    #     ok_or_raise(<int>bmi_run_model(self._bmi))
-
     def finalize(self):
-        status = <int>bmi_finalize(self._bmi)
+        status = <int>self._bmi.finalize(self._bmi)
         ok_or_raise(status)
 
     cpdef object get_component_name(self):
-        ok_or_raise(<int>bmi_get_component_name(self._bmi, self.STR_BUFFER))
+        ok_or_raise(<int>self._bmi.get_component_name(self._bmi, self.STR_BUFFER))
         return self.STR_BUFFER
 
     cpdef int get_input_item_count(self):
         cdef int count = 0
-        ok_or_raise(<int>bmi_get_input_item_count(self._bmi, &count))
+        ok_or_raise(<int>self._bmi.get_input_item_count(self._bmi, &count))
         return count
 
     cpdef int get_output_item_count(self):
         cdef int count = 0
-        ok_or_raise(<int>bmi_get_output_item_count(self._bmi, &count))
+        ok_or_raise(<int>self._bmi.get_output_item_count(self._bmi, &count))
         return count
 
     def get_input_var_names(self):
@@ -153,7 +151,7 @@ cdef class {{ babelized_class }}:
         cdef int count
         cdef int status = 1
 
-        ok_or_raise(<int>bmi_get_input_item_count(self._bmi, &count))
+        ok_or_raise(<int>self._bmi.get_input_item_count(self._bmi, &count))
 
         try:
             names = <char**>malloc(count * sizeof(char*))
@@ -161,7 +159,7 @@ cdef class {{ babelized_class }}:
             for i in range(1, count):
                 names[i] = names[i - 1] + 2048
 
-            ok_or_raise(<int>bmi_get_input_var_names(self._bmi, names))
+            ok_or_raise(<int>self._bmi.get_input_var_names(self._bmi, names))
 
             for i in range(count):
                 py_names.append(names[i])
@@ -180,7 +178,7 @@ cdef class {{ babelized_class }}:
         cdef int count
         cdef int status = 1
 
-        ok_or_raise(<int>bmi_get_output_item_count(self._bmi, &count))
+        ok_or_raise(<int>self._bmi.get_output_item_count(self._bmi, &count))
 
         try:
             names = <char**>malloc(count * sizeof(char*))
@@ -188,7 +186,7 @@ cdef class {{ babelized_class }}:
             for i in range(1, count):
                 names[i] = names[i - 1] + 2048
 
-            ok_or_raise(<int>bmi_get_output_var_names(self._bmi, names))
+            ok_or_raise(<int>self._bmi.get_output_var_names(self._bmi, names))
 
             for i in range(count):
                 py_names.append(names[i])
@@ -202,57 +200,57 @@ cdef class {{ babelized_class }}:
 
     cpdef int get_var_grid(self, name):
         cdef int gid
-        ok_or_raise(<int>bmi_get_var_grid(self._bmi, <char*>name, &gid))
+        ok_or_raise(<int>self._bmi.get_var_grid(self._bmi, <char*>name, &gid))
         return gid
 
     cpdef object get_var_type(self, name):
-        ok_or_raise(<int>bmi_get_var_type(self._bmi, <char*>name, self.STR_BUFFER))
+        ok_or_raise(<int>self._bmi.get_var_type(self._bmi, <char*>name, self.STR_BUFFER))
         return DTYPE_C_TO_PY[self.STR_BUFFER]
 
     cpdef object get_var_units(self, name):
-        ok_or_raise(<int>bmi_get_var_units(self._bmi, <char*>name, self.STR_BUFFER))
+        ok_or_raise(<int>self._bmi.get_var_units(self._bmi, <char*>name, self.STR_BUFFER))
         return self.STR_BUFFER
 
     cpdef int get_var_itemsize(self, name):
         cdef int itemsize
-        ok_or_raise(<int>bmi_get_var_itemsize(self._bmi, <char*>name, &itemsize))
+        ok_or_raise(<int>self._bmi.get_var_itemsize(self._bmi, <char*>name, &itemsize))
         return itemsize
 
     cpdef object get_var_location(self, name):
-        ok_or_raise(<int>bmi_get_var_location(self._bmi, <char*>name, self.STR_BUFFER))
+        ok_or_raise(<int>self._bmi.get_var_location(self._bmi, <char*>name, self.STR_BUFFER))
         return self.STR_BUFFER
 
     cpdef int get_var_nbytes(self, name):
         cdef int nbytes
-        ok_or_raise(<int>bmi_get_var_nbytes(self._bmi, <char*>name, &nbytes))
+        ok_or_raise(<int>self._bmi.get_var_nbytes(self._bmi, <char*>name, &nbytes))
         return nbytes
 
     cpdef double get_current_time(self):
         cdef double time
-        ok_or_raise(<int>bmi_get_current_time(self._bmi, &time))
+        ok_or_raise(<int>self._bmi.get_current_time(self._bmi, &time))
         return time
 
     cpdef double get_start_time(self):
         cdef double time
-        ok_or_raise(<int>bmi_get_start_time(self._bmi, &time))
+        ok_or_raise(<int>self._bmi.get_start_time(self._bmi, &time))
         return time
 
     cpdef double get_end_time(self):
         cdef double time
-        ok_or_raise(<int>bmi_get_end_time(self._bmi, &time))
+        ok_or_raise(<int>self._bmi.get_end_time(self._bmi, &time))
         return time
 
     cpdef object get_time_units(self):
-        ok_or_raise(<int>bmi_get_time_units(self._bmi, self.STR_BUFFER))
+        ok_or_raise(<int>self._bmi.get_time_units(self._bmi, self.STR_BUFFER))
         return self.STR_BUFFER
 
     cpdef double get_time_step(self):
         cdef double time
-        ok_or_raise(<int>bmi_get_time_step(self._bmi, &time))
+        ok_or_raise(<int>self._bmi.get_time_step(self._bmi, &time))
         return time
 
     cpdef get_value(self, name, np.ndarray buff):
-        ok_or_raise(<int>bmi_get_value(self._bmi, <char*>name, buff.data))
+        ok_or_raise(<int>self._bmi.get_value(self._bmi, <char*>name, buff.data))
         return buff
 
     cpdef get_value_ptr(self, name):
@@ -264,22 +262,22 @@ cdef class {{ babelized_class }}:
         return np.asarray(<np.float_t[:size]>ptr)
 
     cpdef set_value(self, name, np.ndarray buff):
-        ok_or_raise(<int>bmi_set_value(self._bmi, <char*>name, buff.data))
+        ok_or_raise(<int>self._bmi.set_value(self._bmi, <char*>name, buff.data))
         return buff
 
     cpdef int get_grid_rank(self, gid):
         cdef int rank
-        ok_or_raise(<int>bmi_get_grid_rank(self._bmi, gid, &rank))
+        ok_or_raise(<int>self._bmi.get_grid_rank(self._bmi, gid, &rank))
         return rank
 
     cpdef int get_grid_size(self, gid):
         cdef int size
-        ok_or_raise(<int>bmi_get_grid_size(self._bmi, gid, &size))
+        ok_or_raise(<int>self._bmi.get_grid_size(self._bmi, gid, &size))
         return size
 
     cpdef int get_grid_node_count(self, gid):
         cdef int size
-        ok_or_raise(<int>bmi_get_grid_size(self._bmi, gid, &size))
+        ok_or_raise(<int>self._bmi.get_grid_size(self._bmi, gid, &size))
         return size
 
     cpdef object get_grid_type(self, gid):
@@ -287,15 +285,15 @@ cdef class {{ babelized_class }}:
         return self.STR_BUFFER
 
     cpdef get_grid_shape(self, int gid, np.ndarray[int, ndim=1] shape):
-        ok_or_raise(<int>bmi_get_grid_shape(self._bmi, gid, &shape[0]))
+        ok_or_raise(<int>self._bmi.get_grid_shape(self._bmi, gid, &shape[0]))
         return shape
 
     cpdef get_grid_spacing(self, int gid, np.ndarray[double, ndim=1] spacing):
-        ok_or_raise(<int>bmi_get_grid_spacing(self._bmi, gid, &spacing[0]))
+        ok_or_raise(<int>self._bmi.get_grid_spacing(self._bmi, gid, &spacing[0]))
         return spacing
 
     cpdef get_grid_origin(self, int gid, np.ndarray[double, ndim=1] origin):
-        ok_or_raise(<int>bmi_get_grid_origin(self._bmi, gid, &origin[0]))
+        ok_or_raise(<int>self._bmi.get_grid_origin(self._bmi, gid, &origin[0]))
         return origin
 
 {% endfor %}
