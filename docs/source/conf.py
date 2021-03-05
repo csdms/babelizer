f126a17f5835e5f5b46664df4161bab27dcc6b54
mcflugen <mcflugen@gmail.com>
2021-03-05 14:56:23 -0700

    reformat with black

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,16 +18,16 @@ import pkg_resources
 
 
 # The master toctree document.
-master_doc = 'index'
+master_doc = "index"
 
 # -- Project information -----------------------------------------------------
 
-project = 'babelizer'
-author = 'Community Surface Dynamics Modeling System'
+project = "babelizer"
+author = "Community Surface Dynamics Modeling System"
 version = pkg_resources.get_distribution("babelizer").version
 release = version
 this_year = datetime.date.today().year
-copyright = '%s, %s' % (this_year, author)
+copyright = "%s, %s" % (this_year, author)
 
 
 # -- General configuration ---------------------------------------------------
@@ -35,10 +35,10 @@ copyright = '%s, %s' % (this_year, author)
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon']
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon"]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -51,12 +51,12 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "alabaster"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
@@ -75,5 +75,5 @@ html_sidebars = {
         "links.html",
         "sourcelink.html",
         "searchbox.html",
-    ]
+    ],
 }
--- docs/source/examples/pymt_heatc_ex.py
+++ docs/source/examples/pymt_heatc_ex.py
@@ -8,55 +8,55 @@ m = HeatModel()
 print(m.name)
 
 # Call setup, then initialize the model.
-args = m.setup('.')
+args = m.setup(".")
 m.initialize(*args)
 
 # List the model's exchange items.
-print('Number of input vars:', len(m.input_var_names))
+print("Number of input vars:", len(m.input_var_names))
 for var in m.input_var_names:
-    print(' - {}'.format(var))
-print('Number of output vars:', len(m.output_var_names))
+    print(" - {}".format(var))
+print("Number of output vars:", len(m.output_var_names))
 for var in m.output_var_names:
-    print(' - {}'.format(var))
+    print(" - {}".format(var))
 
 # Get variable info.
 var_name = m.output_var_names[0]
-print('Variable {}'.format(var_name))
-print(' - variable type:', m.var_type(var_name))
-print(' - units:', m.var_units(var_name))
-print(' - itemsize:', m.var_itemsize(var_name))
-print(' - nbytes:', m.var_nbytes(var_name))
-print(' - location:', m.var_location(var_name))
+print("Variable {}".format(var_name))
+print(" - variable type:", m.var_type(var_name))
+print(" - units:", m.var_units(var_name))
+print(" - itemsize:", m.var_itemsize(var_name))
+print(" - nbytes:", m.var_nbytes(var_name))
+print(" - location:", m.var_location(var_name))
 
 # Get grid info for variable.
 grid_id = m.var_grid(var_name)
-print(' - grid id:', grid_id)
-print(' - grid type:', m.grid_type(grid_id))
-print(' - rank:', m.grid_ndim(grid_id))
-print(' - size:', m.grid_node_count(grid_id))
-print(' - shape:', m.grid_shape(grid_id))
+print(" - grid id:", grid_id)
+print(" - grid type:", m.grid_type(grid_id))
+print(" - rank:", m.grid_ndim(grid_id))
+print(" - size:", m.grid_node_count(grid_id))
+print(" - shape:", m.grid_shape(grid_id))
 
 # Get time information from the model.
-print('Start time:', m.start_time)
-print('End time:', m.end_time)
-print('Current time:', m.time)
-print('Time step:', m.time_step)
-print('Time units:', m.time_units)
+print("Start time:", m.start_time)
+print("End time:", m.end_time)
+print("Current time:", m.time)
+print("Time step:", m.time_step)
+print("Time units:", m.time_units)
 
 # Get the initial values of the variable.
-print('Get values of {}...'.format(var_name))
+print("Get values of {}...".format(var_name))
 val = m.var[var_name].data
-print(' - values at time {}:'.format(m.time))
+print(" - values at time {}:".format(m.time))
 print(val)
 
 # Advance the model by one time step.
 m.update()
-print('Update: current time:', m.time)
+print("Update: current time:", m.time)
 
 # Advance the model until a later time.
 m.update_until(5.0)
-print('Update: current time:', m.time)
+print("Update: current time:", m.time)
 
 # Finalize the model.
 m.finalize()
-print('Done.')
+print("Done.")
