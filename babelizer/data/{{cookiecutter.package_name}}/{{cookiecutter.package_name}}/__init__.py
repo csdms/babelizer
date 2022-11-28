e439d4962d9b07ed394f3dd4a10c80173057ffa7
mcflugen <mcflugen@gmail.com>
2022-11-28 16:51:03 -0700

    make the bmi module private

--- babelizer/data/{{cookiecutter.package_name}}/{{cookiecutter.package_name}}/__init__.py
+++ babelizer/data/{{cookiecutter.package_name}}/{{cookiecutter.package_name}}/__init__.py
@@ -1,17 +1,13 @@
-#! /usr/bin/env python
-import pkg_resources
+from ._version import __version__
 
-
-__version__ = pkg_resources.get_distribution("{{ cookiecutter.package_name }}").version
-
-
-from .bmi import (
+from ._bmi import (
 {%- for babelized_class, _ in cookiecutter.components|dictsort %}
     {{ babelized_class }},
 {%- endfor %}
 )
 
 __all__ = [
+    "__version__",
 {%- for babelized_class, _ in cookiecutter.components|dictsort %}
     "{{ babelized_class }}",
 {%- endfor %}
