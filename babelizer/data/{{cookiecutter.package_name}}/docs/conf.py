16cbcad726553d9684585c132f3269989c4100a0
mcflugen <mcflugen@gmail.com>
2022-11-29 15:48:53 -0700

    switch to furo theme for docs

--- babelizer/data/{{cookiecutter.package_name}}/docs/conf.py
+++ babelizer/data/{{cookiecutter.package_name}}/docs/conf.py
@@ -15,7 +15,12 @@
 # relative to the documentation root, use os.path.abspath to make it
 # absolute, like shown here.
 #
+import os
+import pathlib
+
 import {{ cookiecutter.package_name }}
+
+docs_dir = os.path.dirname(__file__)
 
 # -- General configuration ---------------------------------------------
 
@@ -25,19 +30,32 @@ import {{ cookiecutter.package_name }}
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode']
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.todo",
+    "sphinx.ext.coverage",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.ifconfig",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.autosummary",
+    "sphinx_inline_tabs",
+    "sphinx_copybutton",
+    "sphinxcontrib.towncrier",
+]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
 # source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
+source_suffix = ".rst"
 
 # The master toctree document.
-master_doc = 'index'
+master_doc = "index"
 
 # General information about the project.
 project = '{{ cookiecutter.package_name }}'
@@ -63,10 +81,10 @@ language = "en"
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = []
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+pygments_style = "sphinx"
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = False
@@ -77,21 +95,66 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "furo"
+html_title = "{{ cookiecutter.package_name }}"
+
 
 # Theme options are theme-specific and customize the look and feel of a
 # theme further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+    "announcement": None,
+    "source_repository": "https://github.com/{{ cookiecutter.info.github_username }}/{{ cookiecutter.package_name }}/",
+    "source_branch": "main",
+    "source_directory": "docs",
+    "sidebar_hide_name": False,
+    "footer_icons": [
+        {
+            "name": "power",
+            "url": "https://csdms.colorado.edu",
+            "html": """
+               <svg stroke="currentColor" fill="currentColor" stroke-width="0" version="1.1" viewBox="0 0 16 16" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M6 0l-6 8h6l-4 8 14-10h-8l6-6z"></path></svg>
+               <b><i>Powered by CSDMS</i></b>
+            """,
+            "class": "",
+        },
+    ],
+}
+
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
 
 # -- Options for HTMLHelp output ---------------------------------------
 
 # Output file base name for HTML help builder.
 htmlhelp_basename = "{{ cookiecutter.package_name }}doc"
+
+# -- Options for intersphinx extension ---------------------------------------
+
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3", None),
+}
+
+# -- Options for todo extension ----------------------------------------------
+
+# If true, `todo` and `todoList` produce output, else they produce nothing.
+todo_include_todos = True
+
+# -- Options for napoleon extension ----------------------------------------------
+
+napoleon_numpy_docstring = True
+napoleon_google_docstring = False
+napoleon_include_init_with_doc = True
+napoleon_include_special_with_doc = True
+
+
+# -- Options for towncrier_draft extension --------------------------------------------
+
+towncrier_draft_autoversion_mode = "draft"  # or: 'sphinx-release', 'sphinx-version'
+towncrier_draft_include_empty = True
+towncrier_draft_working_directory = pathlib.Path(docs_dir).parent
--- babelizer/data/{{cookiecutter.package_name}}/docs/index.rst
+++ babelizer/data/{{cookiecutter.package_name}}/docs/index.rst
@@ -1,11 +1,33 @@
-Welcome to {{ cookiecutter.package_name }}'s documentation!
-==========={{ '=' * cookiecutter.package_name | length }}=================
+.. image:: _static/logo-light.svg
+   :align: center
+   :scale: 15%
+   :alt: {{ cookiecutter.package_name }}
+   :target: https://{{ cookiecutter.package_name }}.readthedocs.org/
+   :class: only-light
+
+.. image:: _static/logo-dark.svg
+   :align: center
+   :scale: 15%
+   :alt: {{ cookiecutter.package_name }}
+   :target: https://{{ cookiecutter.package_name }}.readthedocs.org/
+   :class: only-dark
+
 .. toctree::
    :maxdepth: 2
-   :caption: Contents:
+   :caption: User Guide
+   :hidden: true
 
    readme
    installation
-   api/index
-..   usage
+   usage
 ..   contributing
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contribute
+   :hidden: true
+
+   api/index
+   authors
+   changelog
+   license
