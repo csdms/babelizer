ee4d98c4f9bc9af7a780eaadeef4e220e99c2b9f
mcflugen <mcflugen@gmail.com>
2024-03-13 14:11:28 -0600

    render file contents in python, not jinja

--- babelizer/data/{{cookiecutter.package_name}}/{{cookiecutter.package_name}}/lib/__init__.py
+++ babelizer/data/{{cookiecutter.package_name}}/{{cookiecutter.package_name}}/lib/__init__.py
@@ -1,11 +1,1 @@
-#! /usr/bin/env python
-
-{% for babelized_class in cookiecutter.components -%}
-from .{{ babelized_class|lower }} import {{ babelized_class }}
-{% endfor %}
-
-__all__ = [
-{%- for babelized_class in cookiecutter.components -%}
-    "{{ babelized_class }}",
-{%- endfor %}
-]
+{{ cookiecutter.files['lib/__init__.py'] }}
