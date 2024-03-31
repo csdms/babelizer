d9b6aee172ae4441333f20ab134706ae011abfa8
mcflugen <mcflugen@gmail.com>
2024-03-31 16:27:46 -0400

    remove calls to cookiecutter package

--- babelizer/render.py
+++ babelizer/render.py
@@ -8,8 +8,6 @@ import sys
 from typing import Any
 
 import git
-from cookiecutter.exceptions import OutputDirExistsException
-from cookiecutter.main import cookiecutter
 
 from babelizer.metadata import BabelMetadata
 
@@ -26,6 +24,7 @@ if sys.version_info >= (3, 11):  # pragma: no cover (PY11+)
 else:  # pragma: no cover (<PY311)
     import tomli as tomllib
 
+from babelizer._cookiecutter import cookiecutter
 from babelizer._datadir import get_datadir
 from babelizer._files.bmi_py import render as render_bmi
 from babelizer._files.gitignore import render as render_gitignore
@@ -37,49 +36,6 @@ from babelizer.errors import RenderError
 
 
 def render(
-    plugin_metadata: BabelMetadata,
-    output: str,
-    template: str | None = None,
-    clobber: bool = False,
-    version: str = "0.1",
-    make_pretty: bool = False,
-) -> str:
-    from babelizer._cookiecutter import cookiecutter
-
-    if template is None:
-        template = get_datadir()
-
-    context = {
-        "files": {
-            "_bmi.py": render_bmi(plugin_metadata),
-            "__init__.py": render_init(plugin_metadata),
-            "lib/__init__.py": render_lib_init(plugin_metadata),
-            ".gitignore": render_gitignore(plugin_metadata),
-            "LICENSE.rst": render_license(plugin_metadata),
-        },
-        "now": datetime.datetime.now(),
-        "package_version": version,
-    } | {k: plugin_metadata[k] for k in plugin_metadata}
-
-    cookiecutter(
-        template,
-        extra_context=context,
-        output_dir=output,
-        no_input=True,
-        overwrite_if_exists=clobber,
-    )
-
-    path = os.path.realpath(output)
-
-    with open(os.path.join(path, "babel.toml"), "w") as fp:
-        plugin_metadata.dump(fp, fmt="toml")
-
-    git.Repo.init(path)
-
-    return path
-
-
-def render_with_cookiecutter(
     plugin_metadata: BabelMetadata,
     output: str,
     template: str | None = None,
@@ -117,78 +73,33 @@ def render_with_cookiecutter(
     if template is None:
         template = get_datadir()
 
-    context = plugin_metadata.as_cookiecutter_context()
-    context["files"] = {
-        "_bmi.py": render_bmi(plugin_metadata),
-        "__init__.py": render_init(plugin_metadata),
-        "lib/__init__.py": render_lib_init(plugin_metadata),
-        ".gitignore": render_gitignore(plugin_metadata),
-        "LICENSE.rst": render_license(plugin_metadata),
-    }
+    context = {
+        "files": {
+            "_bmi.py": render_bmi(plugin_metadata),
+            "__init__.py": render_init(plugin_metadata),
+            "lib/__init__.py": render_lib_init(plugin_metadata),
+            ".gitignore": render_gitignore(plugin_metadata),
+            "LICENSE.rst": render_license(plugin_metadata),
+        },
+        "now": datetime.datetime.now(),
+        "package_version": version,
+    } | {k: plugin_metadata[k] for k in plugin_metadata}
 
-    try:
-        path = render_plugin_repo(
-            template,
-            context=dict(context, package_version=version),
-            output_dir=output,
-            clobber=clobber,
-        )
-    except OutputDirExistsException as err:
-        raise OutputDirExistsError(", ".join(err.args))
+    if os.path.exists(output):
+        raise OutputDirExistsError(output)
+
+    cookiecutter(
+        template,
+        extra_context=context,
+        output_dir=output,
+        no_input=True,
+        overwrite_if_exists=clobber,
+    )
+
+    path = os.path.realpath(output)
 
     with open(os.path.join(path, "babel.toml"), "w") as fp:
         plugin_metadata.dump(fp, fmt="toml")
-
-    if make_pretty and MAKE_PRETTY:
-        prettify_python(path)
-
-    return os.path.realpath(path)
-
-
-def render_plugin_repo(
-    template: str,
-    context: dict[str, Any] | None = None,
-    output_dir: str = ".",
-    clobber: bool = False,
-) -> str:
-    """Render a repository for a pymt plugin.
-
-    Parameters
-    ----------
-    template: str
-        Path (or URL) to the cookiecutter template to use.
-    context: dict, optional
-        Context for the new repository.
-    output_dir : str, optional
-        Name of the directory that will be the new repository.
-    clobber: bool, optional
-        If a like-named repository already exists, overwrite it.
-
-    Returns
-    -------
-    path
-        Absolute path to the newly-created repository.
-    """
-    context = context or {}
-
-    try:
-        cookiecutter(
-            template,
-            extra_context=context,
-            output_dir=output_dir,
-            no_input=True,
-            overwrite_if_exists=clobber,
-        )
-    except OutputDirExistsException as err:
-        raise OutputDirExistsError(", ".join(err.args))
-
-    name = context["package_name"]
-
-    # path = os.path.join(output_dir, "{}".format(context["package_name"]))
-    # if not os.path.isdir(path):
-    path = os.path.join(output_dir, name)
-    if not os.path.isdir(path):
-        raise RenderError(f"error creating {path}")
 
     git.Repo.init(path)
 
